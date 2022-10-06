5a8cc0614eff5c679641cc079c5127077e294a27
DiegOliveros <diegoo.ivan@gmail.com>
2022-10-06 13:43:20 -0500

    2022/10/06

---
+++ Código Clases/20221006/codigo.js
@@ -0,0 +1,15 @@
+40
+37
+1225
+"Esto es una cadena"
+true
+false
+
+null 
+
+undefined
+
+[1,2,3,4,5,6,7,8,9]
+
+
+{objeto:"nombre del objeto", id:"e"}
---
+++ Código Clases/20221006/variables.js
@@ -0,0 +1,16 @@
+var nombre = "Diego";
+
+//
+var codigo;
+
+codigo =12345679;
+
+var objetoVector =["Ana", "juan", "Sebastian"]
+
+var objeto ={
+    nombre: "Diego",
+    apellido: "Oliveros",
+    cod: 80
+
+}
+console.log("Hola mundo desde variables.js");
