03714474c084194fa04492f97228c911034c6b2d
DiegOliveros <diegoo.ivan@gmail.com>
2022-10-11 20:48:54 -0500

    c

--- Código Clases/1.javascript/myScript.js
+++ Código Clases/1.javascript/myScript.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    document.getElementById("online").innerHTML="Hola he sido cambiado localmente";
+    document.getElementById("online").innerHTML="Hola he sido cambiado en línea desde myScript.js";
 	var saludo ="Hola mundo! desde un archivo myScript.js, que se encuentra  en el repo MISION_TIC_CICLO_4, pero dentro de una función";
 window.alert(saludo);
 	console.log("saludo desde online.js consola");
--- Código Clases/imagen/indexjs.js
+++ Código Clases/imagen/indexjs.js
@@ -1,2 +1,2 @@
 //document.getElementById('titulo2').innerHTML="Esto es un título desde un archivo externo";
-console.log("saludo desde indexjs.js consola");
+console.log("saludo 1 desde un archivo indexjs.js por consola");
