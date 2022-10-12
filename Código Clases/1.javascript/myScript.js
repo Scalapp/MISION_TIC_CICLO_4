c32f0031113c6cd9a00808b87644fe759f415046
DiegOliveros <diegoo.ivan@gmail.com>
2022-10-11 20:31:07 -0500

    final

--- Código Clases/1.javascript/myScript.js
+++ Código Clases/1.javascript/myScript.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementById("online").innerHTML="Hola he sido cambiado localmente";
+	var saludo ="Hola mundo! desde un archivo myScript.js, que se encuentra  en el repo MISION_TIC_CICLO_4, pero dentro de una función";
+window.alert(saludo);
+	console.log("saludo desde online.js consola");
 }
