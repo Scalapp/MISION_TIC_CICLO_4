b685ed25e38f90f789f12d6a6e5df6b1811dbef1
DiegOliveros <diegoo.ivan@gmail.com>
2022-10-12 20:17:55 -0500

    2022 / 10 / 12  Changes to be committed: 	modified:   "C\303\263digo Clases/2.variables/var_let_y_const.html" 	new file:   "C\303\263digo Clases/imagen/ciclos.html" 	new file:   "C\303\263digo Clases/imagen/estructura.html" 	new file:   "C\303\263digo Clases/imagen/salida.html" 	new file:   "C\303\263digo Clases/imagen/sj/notas.js"

---
+++ Código Clases/imagen/sj/notas.js
@@ -0,0 +1,17 @@
+contenido=()=>"Hola este es un contenido desde notas.js";
+ 
+reporte=()=>{
+    return "********** REPÓRTE FINAL DE NOTAS **********<br>"+
+    "<br>Estudiante: " + tarea.nombre+      
+    "<br>Fecha: " + tarea.fecha+ 
+    "<br>Descripción: " + tarea.descrip+     
+    "<br>Esta es la nota final: "  + tarea.nota+
+    "<br>****************  FIN  ****************<br>";
+}
+
+
+/*console.log(tarea.nota);
+       console.log(tarea.fecha);
+       console.log(tarea.descrip);
+       console.log(tarea.nombre);
+       console.log(tarea.cambiarnota(4));*/
