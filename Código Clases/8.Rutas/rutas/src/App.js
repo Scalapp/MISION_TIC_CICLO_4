305faf5bdc32a6e3c9aa3e027c28e15ff1166912
DiegOliveros <diegoo.ivan@gmail.com>
2022-10-28 18:45:04 -0500

    2022 10 28 -730

--- Código Clases/8.Rutas/rutas/src/App.js
+++ Código Clases/8.Rutas/rutas/src/App.js
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper!</h1>
+      <h1>Este es mi pequeño comercio!</h1>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
       >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
+        <Link to="/invoices">Incresos</Link> |{" "}
+        <Link to="/expenses">Egresos-gastos</Link>
       </nav>
     </div>
   );
