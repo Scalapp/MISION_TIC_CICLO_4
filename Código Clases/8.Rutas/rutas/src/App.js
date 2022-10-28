1039fa5814722be9bee3826aa9bc1b0b931dee25
DiegOliveros <diegoo.ivan@gmail.com>
2022-10-28 13:33:42 -0500

    rutas

---
+++ Código Clases/8.Rutas/rutas/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Link } from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bookkeeper!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </div>
+  );
+}
