b8bfd6d8de0ad1c1359a3e50252d7d12c2a9d5ca
DiegOliveros <diegoo.ivan@gmail.com>
2022-11-09 12:21:25 -0500

    2022 11 09

--- Código Clases/12.MERN/config/keys.js
+++ Código Clases/12.MERN/config/keys.js
@@ -1,1 +1,3 @@
-module.exports ={mongoURI:"mongodb+srv://mongo:MDkZMTJw79DdiirK@cluster0.hbvwr8z.mongodb.net/test"};
+module.exports ={mongoURI:"mongodb+srv://mongo:yZPNqUjn4UjQRwV9@cluster0.hbvwr8z.mongodb.net/test"};
+
+//mongodb+srv://<username>:<password>@cluster0.hbvwr8z.mongodb.net/test
--- Código Clases/12.MERN/server.js
+++ Código Clases/12.MERN/server.js
@@ -1,8 +1,8 @@
 const express =require("express");
 const mongoose =require("mongoose");
 const bodyParser =require("body-parser");
-const passport = require('passport');
-const users =require("./routes/api/users");
+//const passport = require('passport');
+//const users =require("./routes/api/users");
 
 const app = express();
 
@@ -19,14 +19,14 @@ mongoose.connect(db, {useNewUrlParser:true})
 .catch((err)=>console.log(err));
 
 //sofware intrermedio passport
-app.use(passport.initialize());
+//app.use(passport.initialize());
 
 //Configuración passport
 
-require("./config/passport")(passport);
+//require("./config/passport")(passport);
 
 //Routes
-app.use("/api/users", users);
+//app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>console.log(`El servidor está corriendo en el puerto: ${port}`));
