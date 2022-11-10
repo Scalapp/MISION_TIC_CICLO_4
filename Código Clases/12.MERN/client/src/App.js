d1e7bef84210e33157032e3af46f72b64b6843eb
DiegOliveros <diegoo.ivan@gmail.com>
2022-11-10 17:01:30 -0500

    Correcciones 1

--- Código Clases/12.MERN/client/src/App.js
+++ Código Clases/12.MERN/client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
+import React from 'react';
 
 function App() {
   return (
--- Código Clases/12.MERN/client/src/index.js
+++ Código Clases/12.MERN/client/src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>
+  
 );
 
 reportWebVitals();
